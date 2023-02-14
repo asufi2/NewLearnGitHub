c8ad822bca5750967c3af8cb1d87c7e0e7a3f1ac
asufi2 <95159610+asufi2@users.noreply.github.com>
2023-02-14 12:12:52 -0500

    first commit/saving my 1st java project into GitHub/GitKraken

---
+++ src/main/java/execute/Execute.java
@@ -0,0 +1,7 @@
+package execute;
+
+public class Execute {
+    public static void main(String[] args) {
+        System.out.println("This is practicing GitHub, and uploading the code to a repository in GitKraken");
+    }
+}
